59747a3a6b5d7a1d1b10e729879f831ab7c60374
Tobias Gass <tobiasgass@gmail.com>
2011-06-23 15:47:41 +0200

    removed old files

---
+++ source/Applications/Multiresolution-SegmentationRegistration3D.cxx
@@ -0,0 +1,58 @@
+
+#include <stdio.h>
+#include <iostream>
+
+#include "argstream.h"
+
+#include "SRSConfig.h"
+#include "HierarchicalSRSImageToImageFilter.h"
+#include "Graph.h"
+#include "BaseLabel.h"
+#include "Potential-Registration-Unary.h"
+#include "Potential-Registration-Pairwise.h"
+#include "Potential-Segmentation-Unary.h"
+#include "Potential-SegmentationRegistration-Pairwise.h"
+
+using namespace std;
+using namespace itk;
+
+int main(int argc, char ** argv)
+{
+
+	feenableexcept(FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
+
+	SRSConfig filterConfig;
+	filterConfig.parseParams(argc,argv);
+	//define types.
+	typedef float PixelType;
+	const unsigned int D=3;
+	typedef Image<PixelType,D> ImageType;
+	typedef itk::Vector<float,D> BaseLabelType;
+    typedef SparseRegistrationLabelMapper<ImageType,BaseLabelType> LabelMapperType;
+    typedef UnaryPotentialSegmentation< ImageType > SegmentationUnaryPotentialType;
+    typedef UnaryPotentialRegistration< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
+    typedef PairwisePotentialRegistration< LabelMapperType, ImageType > RegistrationPairwisePotentialType;
+    typedef PairwisePotentialSegmentationRegistration< LabelMapperType, ImageType > SegmentationRegistrationPairwisePotentialType;
+	typedef HierarchicalSRSImageToImageFilter<ImageType,
+        LabelMapperType,
+        RegistrationUnaryPotentialType,
+        SegmentationUnaryPotentialType,
+        RegistrationPairwisePotentialType,
+        SegmentationRegistrationPairwisePotentialType> FilterType;
+    
+	//create filter
+    FilterType::Pointer filter=FilterType::New();
+    filter->setConfig(filterConfig);
+    filter->setFixedImage(ImageUtils<ImageType>::readImage(filterConfig.targetFilename));
+    filter->setMovingImage(ImageUtils<ImageType>::readImage(filterConfig.movingFilename));
+    filter->setMovingSegmentation(ImageUtils<ImageType>::readImage(filterConfig.movingSegmentationFilename));
+    filter->setFixedGradientImage(ImageUtils<ImageType>::readImage(filterConfig.fixedGradientFilename));
+
+	clock_t start = clock();
+	//DO IT!
+	filter->Update();
+	clock_t end = clock();
+	float t = (float) ((double)(end - start) / CLOCKS_PER_SEC);
+	std::cout<<"Finished computation after "<<t<<" seconds"<<std::endl;
+	return 1;
+}
