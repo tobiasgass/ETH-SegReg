e80548ad26148c04e6ba8a95b36c47c056c7c13a
Tobias Gass <tobiasgass@gmail.com>
2011-10-24 17:26:44 +0200

    correct file

--- source/Models/SubsamplingGraph.h
+++ source/Models/SubsamplingGraph.h
@@ -45,7 +45,7 @@ namespace itk{
                           TUnarySegmentationFunction, 
                           TPairwiseSegmentationFunction,
                           TPairwiseSegmentationRegistrationFunction,
-                           TLabelMapper> Superclass;
+                          TLabelMapper> Superclass;
         itkNewMacro(Self);
 
         //    typedef  itk::ImageToimageFilter<TImage,TImage> Superclass;
@@ -83,76 +83,69 @@ namespace itk{
     
     protected:
         //here we store the necessary information per node
-        struct nodeInformation{
+        struct nodeInformation {
             //mapping
-            vector<LabelType> indexToSubsampledDisplacementMapping;
+            std::vector<RegistrationLabelType> indexToSubsampledDisplacementMapping;
             //costs
-            vector<double> subsampledNodeCosts;
+            std::vector<double> subsampledNodeCosts;
         };
+        //std::pair< std::vector<RegistrationLabelType>, std::vector<double> >
         //information for all nodes
-        vector<nodeInformation> m_nodeMappingInfo;
+        std::vector< nodeInformation > m_nodeMappingInfo;
     public:
 
         void initGraph(int nGraphNodesPerEdge){
             Superclass::initGraph(nGraphNodesPerEdge);
-            m_nodeMappingInfo= vector<nodeInformation>(this->m_nRegistrationLabels);
+            m_nodeMappingInfo= std::vector<nodeInformation>(this->m_nDisplacementLabels);
             //computing new subsampled labels and label costs
             for (int r=0;r<this->m_nRegistrationNodes;++r){
                 //computing original label costs
-                vector<double> originalRegistrationCosts(this->m_nRegistrationLabels);
-                for (int l=o;l<this->m_nRegistrationLabels;++l){
+                std::vector<double> originalRegistrationCosts(this->m_nDisplacementLabels);
+                for (int l=0;l<this->m_nDisplacementLabels;++l){
                     originalRegistrationCosts[l]=Superclass::getUnaryRegistrationPotential(r,l);
                 }
                 
                 // now compute novel mapping and costs and store in m_nodeMappingInfo
                 //for the beginning it might be safe to leave m_nRegistrationLabels at its old value. otherwise we'd need to do some copying/overwritng to be safe
-                m_nodeMappingInfo.indexToSubsampledDisplacementMapping= vector<LabelType>(this->m_nRegistrationLabels);
-                m_nodeMappingInfo.subSampledNodeCosts=vector<double>(this->m_nRegistrationLabels);
+                m_nodeMappingInfo[r].indexToSubsampledDisplacementMapping= std::vector< RegistrationLabelType>(this->m_nDisplacementLabels);
+                m_nodeMappingInfo[r].subsampledNodeCosts=std::vector<double>(this->m_nDisplacementLabels);
                 
                 //actually fill these arrays :D
             }
         }
 
       
+        //this is now all interfacing stuff i needed to rewrite because of the optimizer, don't bother :)
         double getUnaryRegistrationPotential(int nodeIndex,int labelIndex){
-            IndexType imageIndex=getImageIndexFromCoarseGraphIndex(nodeIndex);
-          
             double result=m_nodeMappingInfo[nodeIndex].subsampledNodeCosts[labelIndex];
             return result/this->m_nRegistrationNodes;
         }
      
         double getPairwiseRegistrationPotential(int nodeIndex1, int nodeIndex2, int labelIndex1, int labelIndex2){
-            IndexType graphIndex1=getImageIndexFromCoarseGraphIndex(nodeIndex1);
+            IndexType graphIndex1=this->getImageIndexFromCoarseGraphIndex(nodeIndex1);
             RegistrationLabelType l1=m_nodeMappingInfo[nodeIndex1].indexToSubsampledDisplacementMapping[labelIndex1];
-            l1=LabelMapperType::scaleDisplacement(l1,getDisplacementFactor());
-            IndexType graphIndex2=getImageIndexFromCoarseGraphIndex(nodeIndex2);
+            l1=LabelMapperType::scaleDisplacement(l1,this->getDisplacementFactor());
+            IndexType graphIndex2=this->getImageIndexFromCoarseGraphIndex(nodeIndex2);
             RegistrationLabelType l2=m_nodeMappingInfo[nodeIndex2].indexToSubsampledDisplacementMapping[labelIndex2];
-            l2=LabelMapperType::scaleDisplacement(l2,getDisplacementFactor());
+            l2=LabelMapperType::scaleDisplacement(l2,this->getDisplacementFactor());
             return this->m_pairwiseRegFunction->getPotential(graphIndex1, graphIndex2, l1,l2)/this->m_nRegEdges;
         };
       
         //#define MULTISEGREGNEIGHBORS
         inline double getPairwiseRegSegPotential(int nodeIndex1, int nodeIndex2, int labelIndex1, int segmentationLabel){
     
-            IndexType imageIndex=getImageIndex(nodeIndex2);
+            IndexType imageIndex=this->getImageIndex(nodeIndex2);
             //compute distance between center index and patch index
             double weight=1.0;
-            //#ifdef MULTISEGREGNEIGHBORS
-#if 1
-            IndexType graphIndex=getImageIndexFromCoarseGraphIndex(nodeIndex1);
+            IndexType graphIndex=this->getImageIndexFromCoarseGraphIndex(nodeIndex1);
             double dist=1;
             for (unsigned int d=0;d<this->m_dim;++d){
-                //            std::cout<<dist<<" "<<graphIndex[d]-imageIndex[d]<<" "<<std::endl;
                 dist*=1.0-fabs((1.0*graphIndex[d]-imageIndex[d])/(this->m_gridPixelSpacing[d]));
             }
-            //       if (dist<0.1) dist=0.1;
             weight=dist;
-#endif
-            //        if (true){ std::cout<<graphIndex<<" "<<imageIndex<<" "<<this->m_gridPixelSpacing<<" "<<weight<<std::endl;}
             RegistrationLabelType registrationLabel=m_nodeMappingInfo[nodeIndex1].indexToSubsampledDisplacementMapping[labelIndex1];
-            registrationLabel=LabelMapperType::scaleDisplacement(registrationLabel,getDisplacementFactor());
+            registrationLabel=LabelMapperType::scaleDisplacement(registrationLabel,this->getDisplacementFactor());
             return weight*this->m_pairwiseSegRegFunction->getPotential(imageIndex,imageIndex,registrationLabel,segmentationLabel)/this->m_nSegRegEdges;
-            //        return this->m_pairwiseSegRegFunction->getPotential(graphIndex,imageIndex,registrationLabel,segmentationLabel)/this->m_nSegRegEdges;
         }
 
         
