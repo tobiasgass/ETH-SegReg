8a958a0716962fb9532b3a8c01a202d76e4512d2
Tobias Gass <tobiasgass@gmail.com>
2013-05-08 15:09:37 +0200

    removed accidental out comments of crucial code

--- source/Applications/Multiresolution-SRS3D.cxx
+++ source/Applications/Multiresolution-SRS3D.cxx
@@ -32,7 +32,7 @@ int main(int argc, char ** argv)
     }
     logSetStage("Init");
 
-      //define types.
+    //define types.
 	
     typedef short int PixelType;
 	const unsigned int D=3;
@@ -55,7 +55,7 @@ int main(int argc, char ** argv)
     //typedef SegmentationClassifier<ImageType> ClassifierType;
     //typedef HandcraftedBoneSegmentationClassifierMarcel<ImageType> ClassifierType;
     //typedef UnaryPotentialSegmentationClassifier< ImageType, ClassifierType > SegmentationUnaryPotentialType;
-        // typedef UnaryPotentialSegmentationBoneMarcel< ImageType > SegmentationUnaryPotentialType;
+    typedef UnaryPotentialSegmentationBoneMarcel< ImageType > SegmentationUnaryPotentialType;
     // //typedef UnaryPotentialSegmentationUnsignedBoneMarcel< ImageType > SegmentationUnaryPotentialType;
     // //typedef     UnaryPotentialSegmentation< ImageType > SegmentationUnaryPotentialType;
     
@@ -64,110 +64,110 @@ int main(int argc, char ** argv)
     // //typedef UnaryPotentialNewSegmentationClassifier< ImageType, ClassifierType > SegmentationUnaryPotentialType;
 
     // //pairwise seg
-    // //    typedef UnaryPotentialSegmentation< ImageType > SegmentationUnaryPotentialType;
     // typedef SmoothnessClassifierGradient<ImageType> SegmentationSmoothnessClassifierType;
     // //typedef SmoothnessClassifierGradientContrast<ImageType> SegmentationSmoothnessClassifierType;
     // //typedef SmoothnessClassifierFullMultilabelPosterior<ImageType> SegmentationSmoothnessClassifierType;
     // //typedef CachingPairwisePotentialSegmentationClassifier<ImageType,SegmentationSmoothnessClassifierType> SegmentationPairwisePotentialType;
-    // typedef PairwisePotentialSegmentationMarcel<ImageType> SegmentationPairwisePotentialType;
+    typedef PairwisePotentialSegmentationMarcel<ImageType> SegmentationPairwisePotentialType;
     
     // //reg
-    // //typedef UnaryPotentialRegistrationSAD< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
     // //typedef FastUnaryPotentialRegistrationSAD< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
-    // typedef FastUnaryPotentialRegistrationNCC< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
+    typedef FastUnaryPotentialRegistrationNCC< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
     // //typedef FastUnaryPotentialRegistrationSSD< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
     // //typedef UnaryPotentialRegistrationNCCWithBonePrior< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
     // //typedef UnaryPotentialRegistrationNCCWithDistanceBonePrior< LabelMapperType, ImageType > RegistrationUnaryPotentialType;
-    // typedef PairwisePotentialRegistration< LabelMapperType, ImageType > RegistrationPairwisePotentialType;
-    // typedef PairwisePotentialCoherence< ImageType > CoherencePairwisePotentialType;
+    
+    typedef PairwisePotentialRegistration< LabelMapperType, ImageType > RegistrationPairwisePotentialType;
+    
+    typedef PairwisePotentialCoherence< ImageType > CoherencePairwisePotentialType;
     // //typedef PairwisePotentialSigmoidCoherence< ImageType > CoherencePairwisePotentialType;
     // //typedef PairwisePotentialCoherenceBinary< ImageType > CoherencePairwisePotentialType;
     // //typedef PairwisePotentialBoneCoherence<  ImageType > CoherencePairwisePotentialType;
     // //typedef FastRegistrationGraphModel<
     // //    typedef SortedSubsamplingGraphModel<
-    // typedef FastGraphModel<
-    //     ImageType,
-    //     RegistrationUnaryPotentialType,
-    //     RegistrationPairwisePotentialType,
-    //     SegmentationUnaryPotentialType,
-    //     SegmentationPairwisePotentialType,
-    //     CoherencePairwisePotentialType,
-    //     LabelMapperType>        GraphType;
+    typedef FastGraphModel<
+        ImageType,
+        RegistrationUnaryPotentialType,
+        RegistrationPairwisePotentialType,
+        SegmentationUnaryPotentialType,
+        SegmentationPairwisePotentialType,
+        CoherencePairwisePotentialType,
+        LabelMapperType>        GraphType;
     
-	// typedef HierarchicalSRSImageToImageFilter<GraphType>        FilterType;    
-	// //create filter
-    // FilterType::Pointer filter=FilterType::New();
-    // filter->setConfig(&filterConfig);
-    // logSetStage("IO");
-    // logSetVerbosity(filterConfig.verbose);
-    // LOG<<"Loading target image :"<<filterConfig.targetFilename<<std::endl;
-    // ImagePointerType targetImage=ImageUtils<ImageType>::readImage(filterConfig.targetFilename);
-    // if (!targetImage) {LOG<<"failed!"<<endl; exit(0);}
-    // LOG<<"Loading atlas image :"<<filterConfig.atlasFilename<<std::endl;
-    // ImagePointerType atlasImage;
-    // if (filterConfig.atlasFilename!="") atlasImage=ImageUtils<ImageType>::readImage(filterConfig.atlasFilename);
-    // if (!atlasImage) {LOG<<"Warning: no atlas image loaded!"<<endl;
-    //     LOG<<"Loading atlas segmentation image :"<<filterConfig.atlasSegmentationFilename<<std::endl;}
-    // ImagePointerType atlasSegmentation;
-    // if (filterConfig.atlasSegmentationFilename !="")atlasSegmentation=ImageUtils<ImageType>::readImage(filterConfig.atlasSegmentationFilename);
-    // if (!atlasSegmentation) {LOG<<"Warning: no atlas segmentation loaded!"<<endl; }
-    // logResetStage;
-    // logSetStage("Preprocessing");
-    // //preprocessing 1: gradients
-    // ImagePointerType targetGradient, atlasGradient;
-    // ImagePointerType tissuePrior;
-    // if (filterConfig.segment){
-    //     if (filterConfig.targetGradientFilename!=""){
-    //         targetGradient=(ImageUtils<ImageType>::readImage(filterConfig.targetGradientFilename));
-    //     }else{
-    //         targetGradient=Preprocessing<ImageType>::computeSheetness(targetImage);
-    //         LOGI(8,ImageUtils<ImageType>::writeImage("targetsheetness.nii",targetGradient));
-                 
-    //     }
-    //     if (filterConfig.atlasGradientFilename!=""){
-    //         atlasGradient=(ImageUtils<ImageType>::readImage(filterConfig.atlasGradientFilename));
-    //     }else{
-    //         if (atlasImage.IsNotNull()){
-    //             atlasGradient=Preprocessing<ImageType>::computeSheetness(atlasImage);
-    //             LOGI(8,ImageUtils<ImageType>::writeImage("atlassheetness.nii",atlasGradient));
-    //         }
-    //     }
-        
-    //     if (filterConfig.useTissuePrior){
-    //         tissuePrior=Preprocessing<ImageType>::computeSoftTissueEstimate(targetImage);
-    //     }
-    //     //preprocessing 2: multilabel
-    //     if (filterConfig.computeMultilabelAtlasSegmentation){
-    //         atlasSegmentation=FilterUtils<ImageType>::computeMultilabelSegmentation(atlasSegmentation);
-    //         filterConfig.nSegmentations=5;//TODO!!!!
-    //     }
-    // }
-    // logResetStage;
+    typedef HierarchicalSRSImageToImageFilter<GraphType>        FilterType;    
+    //create filter
+    FilterType::Pointer filter=FilterType::New();
+    filter->setConfig(&filterConfig);
+    logSetStage("IO");
+    logSetVerbosity(filterConfig.verbose);
+    LOG<<"Loading target image :"<<filterConfig.targetFilename<<std::endl;
+    ImagePointerType targetImage=ImageUtils<ImageType>::readImage(filterConfig.targetFilename);
+    if (!targetImage) {LOG<<"failed!"<<endl; exit(0);}
+    LOG<<"Loading atlas image :"<<filterConfig.atlasFilename<<std::endl;
+    ImagePointerType atlasImage;
+    if (filterConfig.atlasFilename!="") atlasImage=ImageUtils<ImageType>::readImage(filterConfig.atlasFilename);
+    if (!atlasImage) {LOG<<"Warning: no atlas image loaded!"<<endl;
+        LOG<<"Loading atlas segmentation image :"<<filterConfig.atlasSegmentationFilename<<std::endl;}
+    ImagePointerType atlasSegmentation;
+    if (filterConfig.atlasSegmentationFilename !="")atlasSegmentation=ImageUtils<ImageType>::readImage(filterConfig.atlasSegmentationFilename);
+    if (!atlasSegmentation) {LOG<<"Warning: no atlas segmentation loaded!"<<endl; }
+    logResetStage;
+    logSetStage("Preprocessing");
+    //preprocessing 1: gradients
+    ImagePointerType targetGradient, atlasGradient;
+    ImagePointerType tissuePrior;
+    if (filterConfig.segment){
+        if (filterConfig.targetGradientFilename!=""){
+            targetGradient=(ImageUtils<ImageType>::readImage(filterConfig.targetGradientFilename));
+        }else{
+            targetGradient=Preprocessing<ImageType>::computeSheetness(targetImage);
+            LOGI(8,ImageUtils<ImageType>::writeImage("targetsheetness.nii",targetGradient));
+           
+        }
+        if (filterConfig.atlasGradientFilename!=""){
+            atlasGradient=(ImageUtils<ImageType>::readImage(filterConfig.atlasGradientFilename));
+        }else{
+            if (atlasImage.IsNotNull()){
+                atlasGradient=Preprocessing<ImageType>::computeSheetness(atlasImage);
+                LOGI(8,ImageUtils<ImageType>::writeImage("atlassheetness.nii",atlasGradient));
+            }
+        }
+  
+        if (filterConfig.useTissuePrior){
+            tissuePrior=Preprocessing<ImageType>::computeSoftTissueEstimate(targetImage);
+        }
+        //preprocessing 2: multilabel
+        if (filterConfig.computeMultilabelAtlasSegmentation){
+            atlasSegmentation=FilterUtils<ImageType>::computeMultilabelSegmentation(atlasSegmentation);
+            filterConfig.nSegmentations=5;//TODO!!!!
+        }
+    }
+    logResetStage;
 
-    // ImagePointerType originalTargetImage=targetImage,originalAtlasImage=atlasImage,originalAtlasSegmentation=atlasSegmentation;
-    // //preprocessing 3: downscaling
-    
-    // if (filterConfig.downScale<1){
-    //     double sigma=1;
-    //     double scale=filterConfig.downScale;
-    //     LOG<<"Resampling images from "<< targetImage->GetLargestPossibleRegion().GetSize()<<" by a factor of"<<scale<<endl;
-    //     targetImage=FilterUtils<ImageType>::LinearResample(targetImage,scale,true);
-    //     if (atlasImage.IsNotNull()) atlasImage=FilterUtils<ImageType>::LinearResample(atlasImage,scale,true);
-    //     if (atlasSegmentation.IsNotNull()) {
-    //         atlasSegmentation=FilterUtils<ImageType>::NNResample((atlasSegmentation),scale,false);
-    //         //ImageUtils<ImageType>::writeImage("testA.nii",atlasSegmentation);
-    //     }
-    //     if (filterConfig.segment){
-    //         targetGradient=FilterUtils<ImageType>::LinearResample(((ImageConstPointerType)targetGradient),scale,true);
-    //         atlasGradient=FilterUtils<ImageType>::LinearResample(((ImageConstPointerType)atlasGradient),scale,true);
-    //         //targetGradient=FilterUtils<ImageType>::NNResample(FilterUtils<ImageType>::gaussian((ImageConstPointerType)targetGradient,sigma),scale);
-    //         //atlasGradient=FilterUtils<ImageType>::NNResample(FilterUtils<ImageType>::gaussian((ImageConstPointerType)atlasGradient,sigma),scale);
-    //         if (filterConfig.useTissuePrior){
-    //             tissuePrior=FilterUtils<ImageType>::LinearResample((targetImage),scale,true);
-    //         }
-    //     }
-    // }
-    // if (filterConfig.segment){
+    ImagePointerType originalTargetImage=targetImage,originalAtlasImage=atlasImage,originalAtlasSegmentation=atlasSegmentation;
+    //preprocessing 3: downscaling
+
+    if (filterConfig.downScale<1){
+        double sigma=1;
+        double scale=filterConfig.downScale;
+        LOG<<"Resampling images from "<< targetImage->GetLargestPossibleRegion().GetSize()<<" by a factor of"<<scale<<endl;
+        targetImage=FilterUtils<ImageType>::LinearResample(targetImage,scale,true);
+        if (atlasImage.IsNotNull()) atlasImage=FilterUtils<ImageType>::LinearResample(atlasImage,scale,true);
+        if (atlasSegmentation.IsNotNull()) {
+            atlasSegmentation=FilterUtils<ImageType>::NNResample((atlasSegmentation),scale,false);
+            //ImageUtils<ImageType>::writeImage("testA.nii",atlasSegmentation);
+        }
+        if (filterConfig.segment){
+            targetGradient=FilterUtils<ImageType>::LinearResample(((ImageConstPointerType)targetGradient),scale,true);
+            atlasGradient=FilterUtils<ImageType>::LinearResample(((ImageConstPointerType)atlasGradient),scale,true);
+            //targetGradient=FilterUtils<ImageType>::NNResample(FilterUtils<ImageType>::gaussian((ImageConstPointerType)targetGradient,sigma),scale);
+            //atlasGradient=FilterUtils<ImageType>::NNResample(FilterUtils<ImageType>::gaussian((ImageConstPointerType)atlasGradient,sigma),scale);
+            if (filterConfig.useTissuePrior){
+                tissuePrior=FilterUtils<ImageType>::LinearResample((targetImage),scale,true);
+            }
+        }
+    }
+    if (filterConfig.segment){
         if (atlasGradient.IsNotNull()) 
             LOGI(10,ImageUtils<ImageType>::writeImage("atlassheetness.nii",atlasGradient));
         LOGI(10,ImageUtils<ImageType>::writeImage("targetsheetness.nii",targetGradient));
