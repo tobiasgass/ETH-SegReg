8c3b5e36b718852e01111b0775b2a8ebb297e578
Tobias <tobiasgass@gmail.com>
2015-02-08 16:57:45 +0100

    library for SRS headers

--- source/SegmentationRegistration/Optimizers/BaseMRF.h
+++ source/SegmentationRegistration/Optimizers/BaseMRF.h
@@ -12,37 +12,37 @@
 #include "Graph.h"
 
 namespace SRS{
-template<class TGraphModel>
-class BaseMRFSolver{
+  template<class TGraphModel>
+    class BaseMRFSolver{
 
-public:
-	//	typedefs
-	typedef TGraphModel GraphModelType;
+  public:
+    //	typedefs
+    typedef TGraphModel GraphModelType;
     typedef typename GraphModelType::Pointer GraphModelPointerType;
-public:
+  public:
     BaseMRFSolver(){};
-	// the constructor only sets member variables
-	BaseMRFSolver(GraphModelPointerType  graphModel,
-                      double unaryRegWeight=1.0, 
-                      double pairwiseRegWeight=1.0, 
-                      double unarySegWeight=1.0, 
-                      double pairwiseSegWeight=1.0, 
-                      double pairwiseSegRegWeight=1.0,
+    // the constructor only sets member variables
+    BaseMRFSolver(GraphModelPointerType  graphModel,
+		  double unaryRegWeight=1.0, 
+		  double pairwiseRegWeight=1.0, 
+		  double unarySegWeight=1.0, 
+		  double pairwiseSegWeight=1.0, 
+		  double pairwiseSegRegWeight=1.0,
                   int vverbose=false){};
 
-	//pure virtual functions have to be implemented by derived classes
+    //pure virtual functions have to be implemented by derived classes
 
-	//create MRF graph, implementatiuon depends on the used optimisation library
-	virtual void createGraph()=0;
-	//finalize initialization and call the optimisation
-	virtual double optimize(int maxIter)=0;
+    //create MRF graph, implementatiuon depends on the used optimisation library
+    virtual void createGraph()=0;
+    //finalize initialization and call the optimisation
+    virtual double optimize(int maxIter)=0;
 
     virtual std::vector<int> getDeformationLabels()=0;
     virtual std::vector<int> getSegmentationLabels()=0;
     virtual double optimizeOneStep(int currentIter , bool & converged)=0;
     virtual void setPotentialCaching(bool b){} 
 
-};//MRFSolver
+  };//MRFSolver
 }//namespace
 
 
