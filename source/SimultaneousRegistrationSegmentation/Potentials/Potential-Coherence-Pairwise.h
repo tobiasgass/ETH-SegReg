2ba00daae7206d46e85cc763449307033cdf6740
Tobias Gass <gasst@vision.ee.ethz.ch>
2015-03-17 01:38:36 +0100

    SRS: disable multilabel coherence potential reduction for aux label

--- source/SimultaneousRegistrationSegmentation/Potentials/Potential-Coherence-Pairwise.h
+++ source/SimultaneousRegistrationSegmentation/Potentials/Potential-Coherence-Pairwise.h
@@ -386,7 +386,7 @@ namespace SRS{
 	    ///do not penalize confusion of background and auxiliary label that strongly?
 	    bool auxiliarySegmentation=(segmentationLabel == this->m_auxiliaryLabel && deformedAtlasSegmentation == 0 ) || (deformedAtlasSegmentation == this->m_auxiliaryLabel && segmentationLabel == 0);
 	    if (auxiliarySegmentation){
-                result=min(result,1.0);
+            //result=min(result,1.0);
                 LOGV(16)<<VAR(result)<<endl;
             }
             result=0.5*result*result;//exp(result)-1;
