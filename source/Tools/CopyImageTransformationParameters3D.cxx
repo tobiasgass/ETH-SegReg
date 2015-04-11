f558eb95997a0a11f7dbcc22334e28ec762a5d6f
Tobias <tobiasgass@gmail.com>
2015-04-11 14:10:45 +0200

    added option to also set direction from the command line

--- source/Tools/CopyImageTransformationParameters3D.cxx
+++ source/Tools/CopyImageTransformationParameters3D.cxx
@@ -29,33 +29,48 @@ int main(int argc, char ** argv)
  
     ArgumentParser * as=new ArgumentParser(argc,argv);
     string inFile, outFile,refFile="";
-    bool spacing=false,origin=false,direction=false;
+    bool copyRefSpacing=false,copyRefOrigin=false,copyRefDirection=false;
+    string direction="";
     as->parameter ("in", inFile, " filename...", true);
     as->parameter ("out", outFile, " filename...", true);
-    as->parameter ("ref", refFile, " filename...", true);
-    as->option ("spacing", spacing, "copy spacing of reference");
-    as->option ("origin", origin, " copy origin of reference.");
-    as->option ("direction", direction, " copy direction of reference.");
-  
+    as->parameter ("ref", refFile, " filename...", false);
+    as->option ("copyRefSpacing", copyRefSpacing, "copy spacing of reference");
+    as->option ("copyRefOrigin", copyRefOrigin, " copy origin of reference.");
+    as->option ("copyRefDirection", copyRefDirection, " copy copyRefDirection of reference.");
+    as->parameter("direction",direction,"set direction matrix",false);
     as->parse();
     
 
     ImagePointerType img = ImageUtils<ImageType>::readImage(inFile);
 
     ImagePointerType outImage=img;
-  
+
+    if (copyRefSpacing || copyRefOrigin || copyRefDirection){
     ImagePointerType reference=ImageUtils<ImageType>::readImage(refFile);
 
-    if (spacing){
+    if (copyRefSpacing){
       outImage->SetSpacing(reference->GetSpacing());
     }
-    if (direction){
+    if (copyRefDirection){
       outImage->SetDirection(reference->GetDirection());
     }
-    if (origin){
+    if (copyRefOrigin){
       outImage->SetOrigin(reference->GetOrigin());
     }
-    
+    }
+    if (direction!=""){
+      string delim(",");
+      std::vector<std::string> entries=split(direction,delim[0]);
+      DirectionType dir;
+      for (int d1=0;d1<D;++d1){
+	for (int d2=0;d2<D;++d2){
+	  std::cout<<" "<<entries[d1*(D)+d2];
+	  const char * c=entries[d1*(D)+d2].c_str();
+	  dir[d2][d1]=atoi(c);
+	}}
+      std::cout<<std::endl;
+      outImage->SetDirection(dir);
+    }
     ImageUtils<ImageType>::writeImage(outFile,outImage);
 
 	return 1;
